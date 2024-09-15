814baed3b8277359b8401157edb3565c32f260f1
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-09-15 19:29:52 -0300

    updated express

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const routes = require('./routes/Routes');
 
 const app = express( );
 app.use(cors( ));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+
+
 
 app.listen(3000, ( )=>{
     console.log('Server connected');
