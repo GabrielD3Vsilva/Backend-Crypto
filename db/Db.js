01ff7164be74470a3aa2bc2211e68151e0b5247c
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-09-16 12:36:36 -0300

    Update Db.js

--- db/Db.js
+++ db/Db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://gbrieloliveira264:1981abcd.@cluster0.vej7o.mongodb.net/")
+mongoose.connect("mongodb+srv://gbrieloliveira264:1981abcd.@cluster0.6cvez.mongodb.net/")
 .then(( )=>console.log('mongoDb Connected'))
 .catch((error)=>console.log(error));
 
