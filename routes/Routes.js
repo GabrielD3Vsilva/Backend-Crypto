1bc27055ecc51373604403d96883b612daa86762
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-09-17 01:08:35 -0300

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -4,8 +4,27 @@ const Db = require('../db/Db');
 
 routes.post('/register', async ( req, res ) => {
     const {name, email, password} = req.body;
-    return res.send('ok');
-   
+
+    const user = await Db.User.findOne({
+        email: email
+    });
+
+    if(!user) {
+        return res.status(400);
+    }
+
+    try {
+        await Db.User.create({
+            name: name,
+            email: email,
+            password: password,
+            isValidateDocuments: false
+        });
+
+        return res.send('ok');
+    } catch (err) {
+        console.error(err);
+    }
 
 })
 
