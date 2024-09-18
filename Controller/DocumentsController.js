6dac4421a5224eb84f8077e0a1844a599dc491ea
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-09-17 22:21:32 -0300

    Update DocumentsController.js

--- Controller/DocumentsController.js
+++ Controller/DocumentsController.js
@@ -5,20 +5,19 @@ const GetDocumentsToValidate = async ( req, res ) => {
     const { cpfNumber, rg, cellPhone, bornDate, id } = req.body;
 
     const isValid = cpf.isValid(cpfNumber);
-
+    
     if(!isValid) {
         return res.status(400).send('Invalid Docs');
     }
 
     try {
-        await Db.User.findByIdAndUpdate({_id: id}, {cpf: cpfNumber, rg: rg, cellPhone: cellPhone, bornDate: bornDate});
+        await Db.User.findByIdAndUpdate(id, {cpf: cpfNumber, rg: rg, cellPhone: cellPhone, bornDate: bornDate});
 
         return res.send(id);
     } catch (err) {
         console.log(err);
+        return res.status(400).send('erro in mongoDb')
     }
-    
-    return res.send(isValid);
 }
 
 module.exports = {GetDocumentsToValidate}
