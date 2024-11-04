f391f641cc78537c59dee0a8e001418a092e2b69
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-11-04 17:14:59 -0300

    Update TransactionController.js

--- Controller/TransactionController.js
+++ Controller/TransactionController.js
@@ -7,52 +7,48 @@ const DogeService = require('./DogeService');
 const EthService = require('./EthService');
 
 async function sendCrypto(req, res) {
-    const { coin, amountInEth, toWallet, pK } = req.body;
+    const { currency, amountInEth, toWallet, pK } = req.body;
 
-    if (!toWallet || !amountInEth || !pK) {
-        return res.status(400).send('Missing required fields');
+
+    let provider = validatePK(currency);
+
+
+    if(provider == 'ETH'){
+        provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/aIBlgH6Ux2NDOmtuz-vQ4nGg-ELApfVf');
+
+        const walletDetails = await EthService.recoverWallet(pKEth);
+        const myAddress = walletDetails.address;
+
+        if (!myAddress) {
+            console.log('You don\'t have a wallet yet!');
+            return res.status(400);
+        }
+
+
+        if (!EthService.addressIsValid(toWallet)) {
+            console.log('Invalid Wallet');
+            return res.status(400);
+        }
+
+
+        try {
+            const tx = await EthService.buildTransaction(toWallet, amountInEth);
+
+            if (!tx) {
+                console.log('Insufficient balance');
+                return res.send('Insufficient balance');
+            }
+            
+            const txReceipt = await EthService.sendTransaction(tx);
+
+            res.send('Transaction successful!', txReceipt);
+        } catch (err) {
+            console.log(err);
+        }
+
+
     }
 
-    let myWallet = WalletService.recoverWallet(pK); // Atualizando para 'let'
-
-    const myAddress = myWallet.address;
-
-    console.log('Your recovered wallet: ');
-    console.log(myAddress);
-
-    if (!myAddress) {
-        console.log('You don\'t have a wallet yet! ');
-        return res.status(400).send('No wallet found');
-    }
-    
-    if (!WalletService.addressIsValid(toWallet)) {
-        console.log('Invalid Wallet');
-        return res.status(400).send('invalid wallet');
-    }
-
-    const bal = await WalletService.getBalance(myAddress);
-    
-    if (!amountInEth) {
-        console.log('Invalid amount');
-        return res.status(400).send('invalid amount');
-    }
-
-    const tx = await WalletService.buildTransaction(toWallet, amountInEth);
-
-    if (!tx) {
-        console.log('Insufficient balance');
-        return res.status(400).send('insufficient balance');
-    }
-
-    try {
-        const txReceipt = await WalletService.sendTransaction(tx);
-        console.log('Transaction successful!');
-        console.log(txReceipt);
-        return res.send(txReceipt);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send('Transaction failed');
-    }
 }
 
 
