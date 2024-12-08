b93b9c00d9967159d48437be088624747d50ecfb
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-12-08 13:07:28 -0300

    Update MercadoPagoService.js

--- Controller/MercadoPagoService.js
+++ Controller/MercadoPagoService.js
@@ -1,10 +1,20 @@
 const { MercadoPagoConfig, Preference } = require('mercadopago');
 const addBalanceToKuCoin = require('./AddBalanceToKuCoin');
 
+const axios = require('axios');
+
+async function getEthPrice() {
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=BRL');
+    return response.data.ethereum.brl;
+}
+
 async function createACheckoutToKucoinApi(req, res) {
     const client = new MercadoPagoConfig({ accessToken: 'TEST-8001012963880387-060323-cdb26a9b2c52877f4a0ae4bc256d8912-1840600103' });
 
     const { amountInEth, ethAddress } = req.body; // Receber dados do frontend
+
+    const ethPrice = await getEthPrice(); // Consultar o valor atual do ETH
+    const finalPrice = ethPrice * amountInEth; // Multiplicar pelo amountInEth
 
     const preference = new Preference(client);
 
@@ -15,7 +25,7 @@ async function createACheckoutToKucoinApi(req, res) {
                 title: 'Compra de ethereum',
                 quantity: 1,
                 currency_id: 'BRL',
-                unit_price: 90,
+                unit_price: finalPrice, // Usar o preço final calculado
             },
         ],
         notification_url: 'https://seu-backend.com/notifications', // URL do seu webhook
@@ -29,6 +39,7 @@ async function createACheckoutToKucoinApi(req, res) {
         return res.send(response.init_point);
     });
 }
+
 
 
   
